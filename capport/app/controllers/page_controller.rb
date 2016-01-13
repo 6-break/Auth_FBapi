63e829e7e327dec5a6c20380085eb136db348cd7
ao <regios0710@live.jp>
2016-01-13 15:29:42 +0900

    comment out bindings.pry

--- capport/app/controllers/page_controller.rb
+++ capport/app/controllers/page_controller.rb
@@ -7,14 +7,14 @@ class PageController < ApplicationController
   #def result
   #end
   def result
-   bindings.pry
+   #bindings.pry
    if session[:previous_url] != check_path && session[:previous_url] != result_path
    redirect_to check_path
    end
   end
   #likesの処理
   def check
-   bindings.pry
+   #bindings.pry
     client = current_user.graph
     events = client.get_connections("me", "events")
     events.each do |event|
