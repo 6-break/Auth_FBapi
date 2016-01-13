bcdaa17bece5bd323983166d44ae91cb607a61ff
ao <regios0710@live.jp>
2016-01-13 19:38:39 +0900

    finally, nothing changed

--- capport/app/controllers/users/omniauth_callbacks_controller.rb
+++ capport/app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,10 +2,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   def facebook
     @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
-
+    #, current_user
     if @user.persisted?
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
       sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
@@ -13,9 +13,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
 end
-
-
-  
 
   # devise :omniauthable, omniauth_providers: [:twitter]
 
