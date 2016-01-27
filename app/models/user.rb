b38f054747e488945535968a3f9ccf190304ad3b
ao <regios0710@live.jp>
2016-01-27 13:17:30 +0900

    delete end before def graph[C

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,14 +17,16 @@ def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
                     )
   end
   user
-  end
 
   def graph
+
    ##account for facebook test user
-  # access_token = 'CAACGmAh9H84BADI7v4P5sXzpxwauqaLZADdt1zFvuISYiNUZAsxAZC33aVC4sUlrgZCRD7GKSx4UCICMsbZABjGf526EZALPzpudHoWvaMIZCsDu6vj8ERX3LilZA6kKFubNTq9cdbIYJ6X9Xi37YKhbtmDGfvvdbHBv70wcU47MMDxpd20kdOIqRuDFjAx2EffsUSNr6BgLbHZA8T4bH2jT0'
+ #  access_token = 'CAACGmAh9H84BADI7v4P5sXzpxwauqaLZADdt1zFvuISYiNUZAsxAZC33aVC4sUlrgZCRD7GKSx4UCICMsbZABjGf526EZALPzpudHoWvaMIZCsDu6vj8ERX3LilZA6kKFubNTq9cdbIYJ6X9Xi37YKhbtmDGfvvdbHBv70wcU47MMDxpd20kdOIqRuDFjAx2EffsUSNr6BgLbHZA8T4bH2jT0'
    # access_token = 'CAACGmAh9H84BAC2t2aSABYuDiLlW8KmnM37cfTNCp1ojBQNcQ7b6XLxDtIQRVToSFmn0sojKTVXLEgYv4LJ77ZAXOE7JUD1P2XZBPpHVZBn5AWgfRaQNZBAQMRWrWuGy8NitdbQnWIhaLS0Gq01jJrRZBEfgECSZCBh20XGUd3CqrHceZA6MaQCXReErGaDKIyGLoKRPgN7ZCAZDZD' 
-    Koala::Facebook::API.new(self.token)
-   # Koala::Facebook::API.new(access_token)
+   Koala::Facebook::API.new(self.token)
+    #puts self.token
+
+  # Koala::Facebook::API.new(access_token)
 
   end
 
