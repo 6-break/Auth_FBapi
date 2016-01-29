d373ee86c533b4ae5a3c9af575d03f342a8bcef3
ao <regios0710@live.jp>
2016-01-29 16:52:59 +0900

    delete oauth_access_token

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ActiveRecord::Base
 def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
   user = User.where(:provider => auth.provider, :uid => auth.uid).first
   unless user
-  
+  #oauth_access_token = user.token
+
 
   user = User.create(#name:     auth.extra.raw_info.name,
                      provider: auth.provider,
@@ -17,23 +18,19 @@ def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
                      email:    auth.info.email,
                      password: Devise.friendly_token[0,20],
                      #token:  　user.token
-                    )
-
-  #token = 
+                    ) 
   end
   user
 end
 
   def graph
-
    ##account for facebook test user
  #  access_token = 'CAACGmAh9H84BADI7v4P5sXzpxwauqaLZADdt1zFvuISYiNUZAsxAZC33aVC4sUlrgZCRD7GKSx4UCICMsbZABjGf526EZALPzpudHoWvaMIZCsDu6vj8ERX3LilZA6kKFubNTq9cdbIYJ6X9Xi37YKhbtmDGfvvdbHBv70wcU47MMDxpd20kdOIqRuDFjAx2EffsUSNr6BgLbHZA8T4bH2jT0'
    #access_token = 'CAACGmAh9H84BAC2t2aSABYuDiLlW8KmnM37cfTNCp1ojBQNcQ7b6XLxDtIQRVToSFmn0sojKTVXLEgYv4LJ77ZAXOE7JUD1P2XZBPpHVZBn5AWgfRaQNZBAQMRWrWuGy8NitdbQnWIhaLS0Gq01jJrRZBEfgECSZCBh20XGUd3CqrHceZA6MaQCXReErGaDKIyGLoKRPgN7ZCAZDZD' 
   # binding.pry
-Koala::Facebook::API.new(self.token)
+  Koala::Facebook::API.new(self.token)
    #Koala::Facebook::API.new(self.token)
     #puts self.token
-
   # Koala::Facebook::API.new(access_token)
 
   end
